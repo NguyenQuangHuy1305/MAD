bd836cc7303787c995d07bb6819c7849657b4833
Huy <quanghuy.nguyen2@griffithuni.edu.au>
2022-03-15 21:54:11 +1000

    Create assignment 1 project

---
+++ Assignment1/Assignment1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment1
+//
+//  Created by Nguyen Quang Huy on 15/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
